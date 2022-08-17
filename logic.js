9da124929a0f72bda0ad7cd3faf423f8de920482
dragosc1 <dragos.ciobanu232@gmail.com>
2022-08-17 15:30:04 +0300

    Added different grid sizes

--- logic.js
+++ logic.js
@@ -1,4 +1,4 @@
-let container = document.querySelector('.container');
+const container = document.querySelector('.container');
 for (let i = 0; i < 16; i++)
     for (let j = 0; j < 16; j++) {
         let cell = document.createElement('div');
@@ -8,7 +8,28 @@ for (let i = 0; i < 16; i++)
         cell.style.border = "1px solid black"
         container.appendChild(cell);
     }
-let grid = document.querySelectorAll('.cell');
+const grid = document.querySelectorAll('.cell');
 grid.forEach(cell => cell.addEventListener('mouseover', function(e) {
     e.target.style.backgroundColor = "gray";
 }));
+const gridSize = document.querySelector('select');
+function selectChange() {
+    const size = gridSize.value;
+    const cellSize = 720 / size;
+    console.log(cellSize);
+    container.innerHTML = '';
+    for (let i = 0; i < size; i++)
+        for (let j = 0; j < size; j++) {
+            let cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.width = `${cellSize}px`;
+            cell.style.height = `${cellSize}px`;
+            cell.style.border = "1px solid black"
+            container.appendChild(cell);
+        }
+    const grid = document.querySelectorAll('.cell');
+    grid.forEach(cell => cell.addEventListener('mouseover', function(e) {
+        e.target.style.backgroundColor = "gray";
+    }));
+}
+gridSize.onchange = selectChange;
